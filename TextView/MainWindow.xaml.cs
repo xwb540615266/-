a5ee21fc35197b9b942dd4294158946ed2df3cf0
文博 薛 <540615266@qq.com>
2019-12-25 12:12:00 +0800

    薛文博2019210514

---
+++ TextView/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UIHelper;
+
+namespace TextView
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model = new MyModel();
+            this.DataContext = Model;
+        }
+        private MyModel Model;
+        private void onOpen_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenTextFileDialog aDlg = new OpenTextFileDialog();
+            if (aDlg.ShowDialog() != true) return;
+            try
+            {
+                Model.Load(aDlg.FileName, aDlg.CurrentEncoding);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void onStartFilte_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                Model.StartFilte();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void onExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void onStartFilte_canExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Model != null && Model.CanStartFilte;
+        }
+
+        private void onOpenImage_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                Model.OpenImage();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ TextView/MyModel.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextView
+{
+    class MyModel : INotifyPropertyChanged
+    {
+        public void Load(string aFileName, Encoding aEncoding)
+        {
+            Text = File.ReadAllText(aFileName, aEncoding);
+            if (!CanStartFilte)
+                FiltedText = Text;
+            else
+                StartFilte();
+        }
+
+        public string Text
+        {
+            get => _Text;
+            set
+            {
+                if (_Text == value) return;
+                _Text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+
+        public string FileText { get; private set; }
+
+        private string _Text;
+
+        public string FiltedText
+        {
+            get => _FiltedText;
+            set
+            {
+                if (_FiltedText == value) return;
+                _FiltedText = value;
+                OnPropertyChanged(nameof(FiltedText));
+            }
+        }
+        private string _FiltedText;
+
+        public string Pattern
+        {
+            get => _Pattern;
+            set
+            {
+                if (_Pattern == value) return;
+                _Pattern = value;
+                OnPropertyChanged(nameof(Pattern));
+            }
+        }
+        private string _Pattern;
+
+        private void OnPropertyChanged(string aPropertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void StartFilte()
+        {
+            string[] aLines = Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder aStringBuilder = new StringBuilder();
+            Regex aRegex = new Regex(Pattern);
+            foreach (var aLine in aLines)
+            {
+                if (aRegex.IsMatch(aLine))
+                {
+                    aStringBuilder.AppendLine(aLine);
+                }
+            }
+            FiltedText = aStringBuilder.ToString();
+        }
+        public bool CanStartFilte =>
+            !string.IsNullOrWhiteSpace(Text) &&
+            !string.IsNullOrWhiteSpace(Pattern);
+
+        private string _ImageName;
+        public string ImageName
+        {
+            get => _ImageName;
+            set
+            {
+                if (_ImageName == value) return;
+                _ImageName = value;
+                OnPropertyChanged(nameof(ImageName));
+            }
+        }
+        public void OpenImage()
+        {
+            OpenFileDialog aDlg = new OpenFileDialog()
+            {
+                Title = "选择图片",
+                Filter = "jpg|*.jpg|jpeg|*.jpeg",
+            };
+            if (aDlg.ShowDialog() != true) return;
+            ImageName = aDlg.FileName;
+        }
+    }
+}
